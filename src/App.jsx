6e3c4d1a52e130a42ef8dbb315c22d8fed3a83d4
Diana Montero <jacque23478@gmail.com>
2024-05-23 15:14:39 -0600

    Formulario-ejemplo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,42 @@ import Navbar from './components/Navbar'
 const App = () => {
   return (
     <>
-      <Navbar />
+    <Navbar />
+    <div className="container mt-3">
+        <div className="row justify-content-center">
+            <h2 className="text-center">Formulario</h2>
+            <div className="col-4">
+                <div className="mb-3">
+                    <label
+                        for="nombre" 
+                        className="form-label">
+                        Nombre
+                    </label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="nombre"
+                        placeholder=""/>
+                </div>
+            </div>
+           
+            </div>
+            <div className="col-4">
+                <div className="mb-3">
+                    <label
+                        for="email" 
+                        className="form-label">
+                        Email
+                    </label>
+                    <input 
+                        type="email" 
+                        className="form-control" 
+                        id="email"
+                        placeholder="pon tu email"/>
+                </div>
+            </div>
+           
+        </div>
     </>
   )
 }
