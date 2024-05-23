11b4d009278eec394802038ab8e9afe307ce0cfc
Diana Montero <jacque23478@gmail.com>
2024-05-23 15:25:23 -0600

    Agregar cuadro de texto y boton

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,67 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from 'react';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <>
-    <Navbar />
-    <div className="container mt-3">
+      <Navbar />
+      <div className="container mt-3">
         <div className="row justify-content-center">
-            <h2 className="text-center">Formulario</h2>
-            <div className="col-4">
-                <div className="mb-3">
-                    <label
-                        for="nombre" 
-                        className="form-label">
-                        Nombre
-                    </label>
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        id="nombre"
-                        placeholder=""/>
-                </div>
+          <h2 className="text-center">Formulario</h2>
+          <div className="col-4">
+            <div className="mb-3">
+              <label htmlFor="nombre" className="form-label">
+                Nombre
+              </label>
+              <input 
+                type="text" 
+                className="form-control" 
+                id="nombre"
+                placeholder="Ingresa tu nombre"
+              />
             </div>
-           
+          </div>
+          <div className="col-4">
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input 
+                type="email" 
+                className="form-control" 
+                id="email"
+                placeholder="Ingresa tu correo"
+              />
             </div>
-            <div className="col-4">
-                <div className="mb-3">
-                    <label
-                        for="email" 
-                        className="form-label">
-                        Email
-                    </label>
-                    <input 
-                        type="email" 
-                        className="form-control" 
-                        id="email"
-                        placeholder="pon tu email"/>
-                </div>
+          </div>
+          <div className="col-8">
+            <div className="mb-3">
+              <label htmlFor="comentarios" className="form-label">
+                Comentarios
+              </label>
+              <textarea
+                className="form-control"
+                id="comentarios"
+                rows="4"
+                placeholder="Escribe tus comentarios aquí"
+              ></textarea>
             </div>
-           
+          </div>
         </div>
+        <div className="row justify-content-center">
+            <div className="col-5">
+                <button 
+                    type="button" 
+                    onclick="agregarFuncion()"
+                    className="btn btn-success w-100">
+                    Agregar</button>
+            </div>
+        </div>
+      </div>
+      
+      
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
